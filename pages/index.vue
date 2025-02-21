1947115ecb220c4bad8174f75427fd2c6e245326
Camilo Caceres <camiloac97@gmail.com>
2025-02-20 23:37:06 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-4xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">Welcome to SyntaxSage</h1>
+        <p class="text-lg">SyntaxSage is a platform that helps you learn programming languages by providing exercises and explanations.</p>
+    </div>
+</template>
