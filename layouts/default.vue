43f368ae1b707beaf2810e779480f70081801981
Camilo Caceres <camiloac97@gmail.com>
2025-02-22 04:09:29 -0500

    feat: add default layout with navbar and background styling

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Navbar from '@/components/ui/Navbar.vue'
+</script>
+<template>
+    <div class="min-h-screen bg-gradient-to-tl from-blue-900 to-slate-900  text-white">
+        <Navbar />
+        <slot />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="container mx-auto py-8">
-    <h1 class="text-3xl font-bold mb-8">Programming Methods Game</h1>
-
-    <div class="flex  ">
    <GameView />
-    </div>
+
   </div>
 </template>
 
