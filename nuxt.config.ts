9fbe5ef51d4dd24c0c189b2e4088108f17b9faa7
Camilo Caceres <camiloac97@gmail.com>
2025-02-22 04:09:43 -0500

    feat: add Montserrat font and update Tailwind configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@pinia/nuxt"],
   compatibilityDate: "2025-02-20",
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap'
+        }
+      ]
+    }
+  }
 })
